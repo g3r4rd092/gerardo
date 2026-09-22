d5e55ee390775f7c3ee3db0ad6ea0d83b3582b5f
GERARDO MANUEL GOMEZ VAZQUEZ <EX545211@telcel.net>
2026-09-22 09:40:46 -0600

    Inicio proyecto

---
+++ src/componentes/contador.jsx
@@ -0,0 +1,11 @@
+import React, {Component } from "react";
+class Contador extends Component {
+
+    render(){
+        return (
+            <h1> Hola willy </h1>
+        );
+    }
+}
+
+export default Contador;
